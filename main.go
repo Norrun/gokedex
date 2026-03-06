6bc2b6371c696e7d5d6549f0c007c0dce020f720
Norrun <trulvens@tutanota.com>
2026-03-06 21:00:15 +0100

    simple repl loop

--- main.go
+++ main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
-
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		rawCommand := scanner.Text()
+		commands := cleanInput(rawCommand)
+		fmt.Printf("Your command was: %s\n", commands[0])
+	}
 }
